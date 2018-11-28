32e3397ebf3262db218792fead3afc0bde70d8cc
esmayk.tillesse <esmaykalves@gmail.com>
2018-11-28 19:23:45 -0300

    Commit Inicial

---
+++ src/main/java/com/loucos/por/macarra/LoucosPorMacarraoApiApplication.java
@@ -0,0 +1,12 @@
+package com.loucos.por.macarra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoucosPorMacarraoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoucosPorMacarraoApiApplication.class, args);
+	}
+}
