af6905e19006731c11f55dabb5c1abbde6a6ff2b
esmayk.tillesse <esmaykalves@gmail.com>
2018-11-30 22:40:01 -0300

    Adiciona entidade de usuario

---
+++ src/main/java/com/loucos/por/macarra/domain/Usuario.java
@@ -0,0 +1,141 @@
+package com.loucos.por.macarra.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.Type;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String login;
+	private String senha;
+	@Type(type = "org.hibernate.type.NumericBooleanType")
+	private boolean ativo;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date ultimoAcesso;
+	@OneToOne
+	@JoinColumn(name = "id_pessoa")
+	private Pessoa pessoa;
+	@JsonBackReference
+	@ManyToMany(mappedBy = "usuarios")
+	private List<Perfil> perfis = new ArrayList<>();
+
+	public Usuario() {
+	}
+
+	public Usuario(Long id, String login, String senha, boolean ativo, Date ultimoAcesso, Pessoa pessoa) {
+		super();
+		this.id = id;
+		this.login = login;
+		this.senha = senha;
+		this.ativo = ativo;
+		this.ultimoAcesso = ultimoAcesso;
+		this.pessoa = pessoa;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Date getUltimoAcesso() {
+		return ultimoAcesso;
+	}
+
+	public void setUltimoAcesso(Date ultimoAcesso) {
+		this.ultimoAcesso = ultimoAcesso;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public List<Perfil> getPerfis() {
+		return perfis;
+	}
+
+	public void setPerfis(List<Perfil> perfis) {
+		this.perfis = perfis;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + "]";
+	}
+}
