ff8766b7e9185ed4f8aa6a53abbd5ffbbb57128a
esmayk.tillesse <esmaykalves@gmail.com>
2018-12-08 22:25:19 -0300

    Altera perfi para um usuario ter um unico perfil

--- src/main/java/com/loucos/por/macarrao/domain/Perfil.java
+++ src/main/java/com/loucos/por/macarrao/domain/Perfil.java
@@ -1,16 +1,14 @@
 package com.loucos.por.macarrao.domain;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -22,13 +20,12 @@ public class Perfil implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String descricao;
+	
 	@JsonBackReference
-	@ManyToMany
-	@JoinTable(name = "perfil_usuario",
-	joinColumns = @JoinColumn(name = "id_perfil"),
-	inverseJoinColumns = @JoinColumn(name = "id_usuario"))
-	private List<Usuario> usuarios = new ArrayList<>();
+	@OneToMany(cascade= CascadeType.ALL, mappedBy = "perfil")
+	private List<Usuario> usuarios;
 
 	public Perfil() {
 	}
